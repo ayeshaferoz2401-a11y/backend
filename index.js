04eacbe7d91450da95844f06c8042adcd149c27f
ayeshaferoz2401-a11y <ayeshaferoz2401@gmail.com>
2026-04-24 10:35:21 +0530

    added CI/CD pipeline

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const SECRET = process.env.JWT_SECRET || "default secret"
 
 app.get("/",(req, res) => {
-    res.send("Hello, Backend is working!");
+    res.send(", Backend is running with env variables!");
 });
+
+
 
 app.listen(PORT, () => {
     console.log("Server running on port" + PORT)
